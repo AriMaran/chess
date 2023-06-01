1e5ce69b619ac353ee266b3d6c10cd463aecbc15
Ari Maran <aricmaran@gmail.com>
2023-06-01 10:53:45 -0400

    King and Rook movement, captured pieces, turns and visual improvement

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
-import boardgame.Board;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -14,31 +15,39 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
-		ChessMatch chess = new ChessMatch();
-		while(true) {
-			try{
+		
+		while (true) {
+			try {
 				UI.clearScreen();
-				UI.printBoard(chess.getPieces());
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				ChessPiece capturedPiece = chess.performChessMove(source, target);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
 			}
-			catch(ChessException e){
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
-				System.out.println("Press enter to continue");
 				sc.nextLine();
 			}
-			catch(InputMismatchException e){
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				System.out.println("Press enter to continue");
 				sc.nextLine();
 			}
 		}
 	}
-
 }
