aa4e9c51d748995408bd606a3ec01319fe3625f1
Ari Maran <aricmaran@gmail.com>
2023-06-05 13:25:43 -0400

    Added Special move Castling. Bug fixes.

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,14 +57,14 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		if(!chessMatch.getCheckMate()) {
-			System.out.println("Waiting Player : " + chessMatch.getCurrentPlayerO());
+			System.out.println("Waiting Player : " + chessMatch.getCurrentPlayer());
 			if(chessMatch.getCheck()) {
 				System.out.println("CHECK!");
 			}
 		}
 		else {
 			System.out.println("CHECKMATE!");
-			System.out.println("Winner: " + chessMatch.getCurrentPlayerO());
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
